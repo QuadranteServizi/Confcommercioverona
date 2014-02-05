fd104c0cf10d0fcaef1d17f02cc26c58dfc0c3c5
QuadranteServizi <ced@quadranteservizi.it>
2014-02-05 12:10:53 +0100

    Nuovo index.js

--- www/js/index.js
+++ www/js/index.js
@@ -1,22 +1,21 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
- alert("1");
+* Licensed to the Apache Software Foundation (ASF) under one
+* or more contributor license agreements. See the NOTICE file
+* distributed with this work for additional information
+* regarding copyright ownership. The ASF licenses this file
+* to you under the Apache License, Version 2.0 (the
+* "License"); you may not use this file except in compliance
+* with the License. You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied. See the License for the
+* specific language governing permissions and limitations
+* under the License.
+*/
 var app = {
     // Application Constructor
     initialize: function() {
@@ -35,6 +34,9 @@ var app = {
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+        var pushNotification = window.plugins.pushNotification;
+        pushNotification.register(app.successHandler, app.errorHandler,{"senderID":"824841663931","ecb":"app.onNotificationGCM"});
+
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
@@ -46,20 +48,10 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
-        var pushNotification = window.plugins.pushNotification;
-        if (device.platform == 'android' || device.platform == 'Android') {
-            //alert("Register called");
-            pushNotification.register(this.successHandler, this.errorHandler,{"senderID":"1073127551296","ecb":"app.onNotificationGCM"});
-        }
-        else {
-            //alert("Register called");
-            pushNotification.register(this.successHandler,this.errorHandler,{"badge":"true","sound":"true","alert":"true","ecb":"app.onNotificationAPN"});
-        }
     },
     // result contains any message sent from the plugin call
     successHandler: function(result) {
-        alert('Callback Success! Result = '+result);
-        
+        alert('Callback Success! Result = '+result)
     },
     errorHandler:function(error) {
         alert(error);
@@ -68,7 +60,7 @@ var app = {
         switch( e.event )
         {
             case 'registered':
-                if ( e.regid.length > 0 )
+              if ( e.regid.length > 0 )
                 {
                 /*
                 var ref = window.open("http://www.confcommercioverona.it/app/notify_newdevice.php?registrationId="+e.regid, '_blank', 'location=yes', 'hidden=yes');
@@ -98,38 +90,20 @@ var app = {
                 
                 }
             break;
- 
+
             case 'message':
-              // this is the actual push notification. its format depends on the data model from the push server
-              //alert(e.message+' msgcnt = '+e.msgcnt);
-              alert(e.message);
-            break;
- 
+                // this is the actual push notification. its format depends on the data model from the push server
+                alert('message = '+e.message+' msgcnt = '+e.msgcnt);
+                break;
+
             case 'error':
-              //alert("errore");
-              console.log("console: errore");
-              alert('GCM error = '+e.msg);
-            break;
- 
+                alert('GCM error = '+e.msg);
+                break;
+
             default:
-              alert('An unknown GCM event has occurred');
-              break;
-        }
-    },
-    onNotificationAPN: function(event) {
-        var pushNotification = window.plugins.pushNotification;
-        alert("Running in JS - onNotificationAPN - Received a notification! " + event.alert);
-        
-        if (event.alert) {
-            navigator.notification.alert(event.alert);
-        }
-        if (event.badge) {
-            pushNotification.setApplicationIconBadgeNumber(this.successHandler, this.errorHandler, event.badge);
-        }
-        if (event.sound) {
-            var snd = new Media(event.sound);
-            snd.play();
+                alert('An unknown GCM event has occurred');
+                break;
         }
     }
+
 };
-  alert("2");
